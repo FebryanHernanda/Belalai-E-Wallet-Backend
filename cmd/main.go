ed7b12933132d5d05efd0b00510c3477351e79d9
M16yusuf <yusufsmd58@gmail.com>
2025-09-24 19:11:07 +0700

    feat (doc): create init swaggo documentation

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"github.com/Belalai-E-Wallet-Backend/internal/routers"
+)
 
+// @title 											Belalai E-Wallet
+// @version 										1.0
+// @description 								E-wallet team belalai
+// @host												127.0.0.1:2409
+// @securityDefinitions.apikey 	JWTtoken
+// @in header
+// @name Authorization
 func main() {
-	fmt.Println("Hello World")
-	fmt.Println("E-Wallet")
+	// manual load ENV
+
+	// Inisialization databae for this project
+
+	// inisialization redish
+
+	// Inisialization engine gin, HTTP framework
+	// !NEED DB
+	router := routers.InitRouter()
+	router.Run(":2409")
 }
