64ed52bff1cfce6d2642c228c2a67baf8354a2e6
Raihaninkam <raihankamil37@gmail.com>
2025-09-25 13:33:23 +0700

    fix(database): move pin to users relation

--- db/migrations/000002_create_profile_table.up.sql
+++ db/migrations/000002_create_profile_table.up.sql
@@ -3,7 +3,6 @@ CREATE TABLE profile (
     profile_picture VARCHAR(255),
     fullname VARCHAR(255),
     phone VARCHAR(20),
-    pin TEXT,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     CONSTRAINT fk_profile_users FOREIGN KEY (users_id) REFERENCES users(id) ON DELETE CASCADE
