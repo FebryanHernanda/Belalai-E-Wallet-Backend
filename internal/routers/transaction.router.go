924174f20853ca1901df5a2dd1127c8b77fed812
Raihaninkam <raihankamil37@gmail.com>
2025-09-26 10:31:30 +0700

    feat(router): add endpoint route for delete and get all history(topup, transfer)

--- internal/routers/transaction.router.go
+++ internal/routers/transaction.router.go
@@ -14,5 +14,7 @@ func InitTransactionRouter(router *gin.Engine, db *pgxpool.Pool, rdb *redis.Clie
 	transactionRepository := repository.NewTransactionRepository(db)
 	transactionHandler := handler.NewTransactionHandler(transactionRepository)
 
-	transactionRouter.GET("", middleware.VerifyToken(rdb), transactionHandler.GetTransactionHistory)
+	transactionRouter.GET("/history", middleware.VerifyToken(rdb), transactionHandler.GetTransactionHistory)
+	transactionRouter.GET("/history/all", middleware.VerifyToken(rdb), transactionHandler.GetAllTransactionHistory)
+	transactionRouter.DELETE("/:id", middleware.VerifyToken(rdb), transactionHandler.DeleteTransaction)
 }
