36ec609f2269b8cc500ce88a1f34b935433122f1
Raihaninkam <raihankamil37@gmail.com>
2025-09-26 10:30:19 +0700

    feat(model): add id, phone number for transaction history model & add DeleteTransactionRequest model

--- internal/models/transaction.model.go
+++ internal/models/transaction.model.go
@@ -4,9 +4,11 @@ package models
 import "time"
 
 type TransactionHistory struct {
+	ID             int       `json:"id" db:"id"`
 	Type           string    `json:"transaction_type" db:"transaction_type"`
 	ProfilePicture string    `json:"profile_picture" db:"profile_picture"`
 	ContactName    string    `json:"contact_name" db:"contact_name"`
+	PhoneNumber    string    `json:"phone_number" db:"phone_number"`
 	Amount         string    `json:"amount" db:"display_amount"`
 	OriginalAmount int       `json:"original_amount" db:"original_amount"`
 	Status         string    `json:"status" db:"status"`
@@ -22,4 +24,8 @@ type TransactionHistoryResponse struct {
 	Success bool                 `json:"success"`
 	Message string               `json:"message"`
 	Data    []TransactionHistory `json:"data,omitempty"`
+}
+
+type DeleteTransactionRequest struct {
+	TransactionID int `json:"transaction_id" uri:"id" binding:"required"`
 }
