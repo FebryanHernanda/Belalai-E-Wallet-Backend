a1b6867abb14df30fcf2ab3fe4004c81ef5a82c5
M16yusuf <yusufsmd58@gmail.com>
2025-09-26 17:22:16 +0700

    feat (feat/transfer) : create endpoint transfer

--- internal/repository/transfer.repository.go
+++ internal/repository/transfer.repository.go
@@ -2,11 +2,13 @@ package repository
 
 import (
 	"context"
+	"errors"
 	"log"
 	"time"
 
 	"github.com/Belalai-E-Wallet-Backend/internal/models"
 	"github.com/Belalai-E-Wallet-Backend/internal/utils"
+	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/redis/go-redis/v9"
 )
@@ -73,4 +75,112 @@ func (ur *TransferRepository) FilterUser(c context.Context, query string, offset
 
 	// return users, and error nil if success
 	return users, nil
+}
+
+// get user pin for validate user on handler
+func (ur *TransferRepository) GetHashedPin(rqCntxt context.Context, senderId int) (models.UserPin, error) {
+	var userPin models.UserPin
+	sql := `SELECT id, pin FROM users WHERE id=$1`
+	if err := ur.db.QueryRow(rqCntxt, sql, senderId).Scan(&userPin.Id, &userPin.Pin); err != nil {
+		log.Println("failed get pin user \ncause :", err)
+		return models.UserPin{}, errors.New("failed get pin user")
+	}
+	return userPin, nil
+}
+
+// transfer transaction
+func (ur *TransferRepository) TransferMoney(rqCntxt context.Context, senderId int, body models.TransferBody) error {
+
+	// using tx transaction postgresql
+	tx, err := ur.db.Begin(rqCntxt)
+	if err != nil {
+		log.Println("Failed to begin DB transaction\nCause: ", err)
+		return err
+	}
+	defer tx.Rollback(rqCntxt)
+
+	// get balance sender and validate pin sender
+	// if balance sender is not enough to do transfer, abort transaction
+	var senderWalletID int
+	var senderBalance float64
+	qBalSender := `SELECT w.id, w.balance FROM wallets w 
+         JOIN users u ON w.user_id = u.id 
+         WHERE u.id = $1 FOR UPDATE`
+	if err := tx.QueryRow(rqCntxt, qBalSender, senderId).Scan(&senderWalletID, &senderBalance); err != nil {
+		if err == pgx.ErrNoRows {
+			log.Println("error no rows or user invalid", err)
+			return errors.New("user invalid or wrong pin inputs")
+		}
+		log.Println("Internal Server Error.\nCause: ", err.Error())
+		return err
+	}
+	// validate if sender balance is have enough money to do transfer
+	if senderBalance < float64(body.Amount) {
+		return errors.New("not enough balance for this transfer")
+	}
+
+	// validate not sending money to self
+	if senderWalletID == body.IdReceiver {
+		return errors.New("can't sending money to yourself")
+	}
+
+	// execute tranfser
+	// update saldo sender
+	now := time.Now()
+	sqlSenderWallet := `UPDATE wallets SET balance = balance - $1, updated_at = $2 WHERE id = $3`
+	values := []any{body.Amount, now, senderId}
+	cmd, err := tx.Exec(rqCntxt, sqlSenderWallet, values...)
+	if err != nil {
+		log.Println("Failed execute query sqlSenderWallet\nCause:", err)
+		return err
+	}
+	if cmd.RowsAffected() == 0 {
+		log.Println("no row effected when UPDATE wallets maybe failed?")
+		return errors.New("no row effected when UPDATE wallets maybe failed?")
+	}
+	// update saldo receiver
+	sqlReceiverWallet := `UPDATE wallets SET balance = balance + $1, updated_at = $2 WHERE id = $3`
+	values = []any{body.Amount, now, body.IdReceiver}
+	cmd, err = tx.Exec(rqCntxt, sqlReceiverWallet, values...)
+	if err != nil {
+		log.Println("Failed execute query sqlReceiverWallet\nCause:", err)
+		return err
+	}
+	if cmd.RowsAffected() == 0 {
+		log.Println("no row effected when UPDATE wallets maybe failed?")
+		return errors.New("no row effected when UPDATE wallets maybe failed?")
+	}
+
+	// insert transfer data
+	var transferID int
+	sqlTansferTable := `INSERT INTO transfer (sender_wallet_id, receiver_wallet_id, amount, transfer_status, notes, created_at, updated_at)
+    VALUES ($1, $2, $3, 'success', $4, $5, $5) RETURNING id`
+	values = []any{senderId, body.IdReceiver, body.Amount, body.Notes, now}
+	if err := tx.QueryRow(rqCntxt, sqlTansferTable, values...).Scan(&transferID); err != nil {
+		log.Println("Failed execute query sqlTansferTable \nCause :", err)
+		return err
+	}
+
+	// insert wallet_transfer
+	sqlTransferWalletTable := `INSERT INTO wallets_transfer (wallets_id, transfer_id) VALUES ($1, $3), ($2, $3)`
+	values = []any{senderId, body.IdReceiver, transferID}
+	cmd, err = tx.Exec(rqCntxt, sqlTransferWalletTable, values...)
+	if err != nil {
+		log.Println("Failed execute query sqlTransferWalletTable\nCause:", err)
+		return err
+	}
+	if cmd.RowsAffected() == 0 {
+		log.Println("no row effected when INSERT INTO wallets_transfer maybe failed?")
+		return errors.New("no row effected when INSERT INTO wallets_transfer maybe failed?")
+	}
+
+	// commit transaction if all query success execute
+	if err := tx.Commit(rqCntxt); err != nil {
+		log.Println("Failed to commit DB transaction\nCause: ", err)
+		return err
+	}
+	log.Println("success to commit DB transaction")
+
+	// error nil if success
+	return nil
 }
