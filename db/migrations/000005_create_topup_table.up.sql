ca8024699f51a9d76bb18ecb5d78d113b34cd46e
Raihaninkam <raihankamil37@gmail.com>
2025-09-28 20:34:56 +0700

    feat(db): add colum deleted_at in topup column

--- db/migrations/000005_create_topup_table.up.sql
+++ db/migrations/000005_create_topup_table.up.sql
@@ -7,5 +7,6 @@ CREATE TABLE topup (
     topup_status topup_status NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    deleted_at TIMESTAMP DEFAULT NULL
     CONSTRAINT fk_topup_payment FOREIGN KEY (payment_id) REFERENCES payment_method(id)
 );
